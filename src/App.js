193cf636b4b880e988c9a35850781f77f746b3e6
Lbquelca <155509293+Lbquelca@users.noreply.github.com>
2024-06-13 11:04:38 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         <code>Hola</code> Soy Juanito
+        </p>
+        <p>
+          Comenzando React
+        </p>
+        <ul>
+          <li>hola</li>
+          <li>hi</li>
+          <li>Adios</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
